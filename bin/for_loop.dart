f8eb103caf10f361191d2a1d9742d81b36568faf
Okelola Joshua <jayokelola341@gmail.com>
2021-07-20 14:38:09 +0100

    added new while loop

--- bin/for_loop.dart
+++ bin/for_loop.dart
@@ -1,3 +1,4 @@
+/*Simple For Loop*/
 void main() {
   for (var i = 0; i < 5; i++) {
     print(i);
--- bin/while_loop.dart
+++ bin/while_loop.dart
@@ -9,5 +9,10 @@ void main() async {
     sum += 2;
     print('\n$sum');
   } while (sum <= 23);
-  
+
+  var Counter = 0;
+  while (Counter < 10) {
+    print('$Counter is X');
+    Counter++;
+  }
 }
