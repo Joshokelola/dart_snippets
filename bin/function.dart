4662c6688b5c4e908dffd2d4374b73ad4cd3b42f
Okelola Joshua <jayokelola341@gmail.com>
2021-07-20 15:08:51 +0100

    some update...

--- bin/function.dart
+++ bin/function.dart
@@ -3,7 +3,17 @@ void main() {
   String simpleFunction(int number) {
     return '$number is a big number';
   }
+
   print(simpleFunction(487));
 
-  
+  ///using optional parameters
+  String FullName(String first, String last, [String? title]) {
+    if (title != null) {
+      return '$title $first $last';
+    } else {
+      return '$first $last';
+    }
+  }
+
+  print(FullName('Joshua', 'Okelola'));
 }
