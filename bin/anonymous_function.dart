ae8ff1509cfb9e614b898c278d1da351ce30c20f
Okelola Joshua <jayokelola341@gmail.com>
2021-07-20 16:01:34 +0100

    added nested function

--- bin/anonymous_function.dart
+++ bin/anonymous_function.dart
@@ -2,6 +2,15 @@ void main() {
   Function newFunction = (int a, int b) {
     return a * b;
   };
-  print(newFunction(1, 2));
-}
+  print(newFunction(34, 2));
 
+  //Returning a function with a function (._.)
+  Function Area = (num height) {
+    /*This is the inner function*/
+    return (num width) {
+      return height * width;
+    };
+  };
+  final height = Area(3);
+  print(height(2));
+}
